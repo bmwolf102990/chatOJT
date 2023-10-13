1cbcb24e0d81b4120847d9443c527211476112b8
Kelci Fowler <143914534+KelciAnn@users.noreply.github.com>
2023-10-13 08:41:59 -0700

    Updated Heading Font

--- client/src/Components/Title/Title.jsx
+++ client/src/Components/Title/Title.jsx
@@ -5,10 +5,11 @@ const Title = () =>{
     return(
         <div className="container-fluid text-center">
             <div className="row">
-               <header className="fs-1 text-white">
+                {/* font changed */}
+               <header className="fs-1 text-white fw-bold">
                 Chatty Kathy
                </header>
-               <p className="fs-3 text-white">What's on your mind?</p>
+               <p className="fs-6 text-white">What's on your mind?</p>
             </div>
         </div>
     )

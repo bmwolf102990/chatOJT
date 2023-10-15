0858273fc7a706c2dfda3ccc0a31f849dbda629e
xxJavaScriptJoeyxx <jomatteucci80@gmail.com>
2023-10-14 23:36:19 -0700

    input component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import UserInput from "../src/components/input/UserInput"
 
 function App() {
   return (
     <div className="App">
+      <UserInput />
      
     </div>
   );
--- client/src/components/messaging/Message.jsx
+++ client/src/components/messaging/Message.jsx
@@ -1,4 +1,8 @@
 import React from "react"
+import "./message.css"
+
+
+
 
 const Message = ({text}) => {
     return (

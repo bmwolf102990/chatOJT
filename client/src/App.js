ac8682008b4b24400165d23048d60166d1a07a64
Shandea <86807815+Shandea@users.noreply.github.com>
2023-10-17 07:55:42 -0700

    Revert "Ckfe 004"

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
-import UserInput from "../src/components/input/UserInput"
 
 function App() {
   return (
     <div className="App">
-      <UserInput />
      
     </div>
   );
